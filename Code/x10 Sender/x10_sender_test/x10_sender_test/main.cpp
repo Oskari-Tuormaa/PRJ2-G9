17e60a96b64a988d10f2f440ea5e83663ef6dcb8
Oskari-Tuormaa <oskaritu@gmail.com>
2019-05-02 15:08:09 +0200

    Added Atmel Project

---
+++ Code/x10 Sender/x10_sender_test/x10_sender_test/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * x10_sender_test.cpp
+ *
+ * Created: 02-05-2019 15:06:07
+ * Author : oskar
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
